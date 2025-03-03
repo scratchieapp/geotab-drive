1a86f6977fd4eac845700939060564a7afc50e33
scratchieapp <james@scratchie.com>
2025-03-03 13:33:52 +1100

    Simplify project to use Next.js exclusively and fix Vercel deployment issues

--- next.config.js
+++ next.config.js
@@ -14,4 +14,28 @@ module.exports = {
   output: 'standalone',
   // Disable unnecessary features
   swcMinify: true,
+  // Add webpack configuration to handle @geotab/zenith properly
+  webpack: (config, { isServer }) => {
+    // Handle CSS, SASS, and less files from @geotab/zenith
+    config.module.rules.push({
+      test: /\.(css|scss|sass|less)$/,
+      use: ['style-loader', 'css-loader'],
+    });
+
+    // Handle font and svg files
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          publicPath: '/_next/static/media/',
+          outputPath: 'static/media/',
+          name: '[name].[hash:8].[ext]',
+        },
+      },
+    });
+
+    return config;
+  },
 };
---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+import '@geotab/zenith/dist/commonStyles/zenith-common.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp; 
